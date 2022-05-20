5fbf96185bba438d2cdc0b2830d12f11f514b05d
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-20 16:28:22 +0300

    Remove build folder

--- build/src/helpers/extractEmails.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1({ text }) {
-    const regex = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-    return text.match(regex);
-}
-exports.default = default_1;
-
-//# sourceMappingURL=extractEmails.js.map
--- build/src/utils/capitalize.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (text) => {
-    if (text.charCodeAt(0) > 64 && text.charCodeAt(0) < 97)
-        return text;
-    return text.charAt(0).toLocaleUpperCase() + text.slice(1);
-};
-
-//# sourceMappingURL=capitalize.js.map
