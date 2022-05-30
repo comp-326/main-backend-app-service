cf1fb23cc7dcf314db7be13d0cbfd9af5146b66c
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-30 12:43:56 +0300

    Update Course module id pass

---
+++ src/app/__tests__/course/queryCourses.test.ts
@@ -0,0 +1,78 @@
+import app from '@exam-cell-app';
+import supertest from 'supertest';
+
+describe('Query courses', () => {
+	test('should get all courses', async () => {
+		try {
+			await supertest(app).post('/api/v1/courses/all').send({
+				name: 'Science',
+				department: '',
+			});
+		} catch (err) {
+			expect(err.status).toMatchInlineSnapshot();
+			expect(err.statusCode).toMatchInlineSnapshot();
+			expect(err.body).toMatchInlineSnapshot();
+		}
+	});
+	test('should  update  course', async () => {
+		try {
+			const response = await supertest(app)
+				.put('/api/v1/courses/update/didjodijddsiojo')
+				.send({
+					name: 'Science',
+				});
+			expect(response.body).toMatchInlineSnapshot(`
+			Object {
+			  "data": Object {},
+			  "message": "Invalid course id required",
+			  "status": "warning",
+			  "statusCode": 400,
+			}
+		`);
+		} catch (err) {
+			expect(err.status).toMatchInlineSnapshot();
+			expect(err.statusCode).toMatchInlineSnapshot();
+			expect(err.body).toMatchInlineSnapshot();
+		}
+	});
+	test('should  get   course by id', async () => {
+		try {
+			const response = await supertest(app).get(
+				'/api/v1/courses/course/didjodijddsiojo',
+			);
+
+			expect(response.body).toMatchInlineSnapshot(`
+			Object {
+			  "data": Object {},
+			  "message": "Cast to ObjectId failed for value \\"didjodijddsiojo\\" (type string) at path \\"_id\\" for model \\"Course\\"",
+			  "status": "error",
+			}
+		`);
+		} catch (err) {
+			expect(err.status).toMatchInlineSnapshot();
+			expect(err.statusCode).toMatchInlineSnapshot();
+			expect(err.body).toMatchInlineSnapshot();
+		}
+	});
+
+	test('should  get department   courses', async () => {
+		try {
+			const response = await supertest(app).get(
+				'/api/v1/courses/department/didjodijddsiojo',
+			);
+
+			expect(response.body).toMatchInlineSnapshot(`
+			Object {
+			  "data": Object {},
+			  "message": "Invalid Department Id",
+			  "status": "warning",
+			  "statusCode": 400,
+			}
+		`);
+		} catch (err) {
+			expect(err.status).toMatchInlineSnapshot();
+			expect(err.statusCode).toMatchInlineSnapshot();
+			expect(err.body).toMatchInlineSnapshot();
+		}
+	});
+});
--- src/features/Course/use-cases/listCoursesById.ts
+++ src/features/Course/use-cases/listCoursesById.ts
@@ -1,5 +1,7 @@
+import { ExpressError } from '@exam-cell-common/errors/ExpressError';
 import courseModel from '../models';
 import { courseRepositoryType } from './../repository';
+import validateMongodbId from '@exam-cell-utils/mongo/ObjectId-validator';
 
 export function makeListCourseByIdUseCase({
 	repository,
@@ -7,6 +9,22 @@ export function makeListCourseByIdUseCase({
 	repository: courseRepositoryType;
 }) {
 	return async (courseId: string) => {
+		if (!courseId) {
+			throw new ExpressError({
+				message: 'Course id is required',
+				statusCode: 400,
+				data: {},
+				status: 'warning',
+			});
+		}
+		if (!validateMongodbId(courseId)) {
+			throw new ExpressError({
+				message: 'Invalid course id',
+				statusCode: 400,
+				data: {},
+				status: 'warning',
+			});
+		}
 		const course = await repository.findCourseById({
 			courseModel: courseModel,
 		})(courseId);
