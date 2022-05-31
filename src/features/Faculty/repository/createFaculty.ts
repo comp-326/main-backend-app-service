b43cec37990d2af36c62a7454d2f26d6fe8fbab9
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-31 10:12:30 +0300

    Build fix

--- src/features/Faculty/repository/createFaculty.ts
+++ src/features/Faculty/repository/createFaculty.ts
@@ -2,7 +2,7 @@
 import { FacultyModelType } from '@exam-cell-features/Faculty/models';
 import { IFaculty } from '@exam-cell-features/Faculty/models/interfaces';
 
-export function createNewDepartment({ model }: { model: FacultyModelType }) {
+export function createNewFaculty({ model }: { model: FacultyModelType }) {
 	return async (facultyData: IFaculty) => {
 		const faculty = await model.create(facultyData);
 
